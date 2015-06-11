9b0f45dd9cb1515a1e4f629459a5fbe3d36b84da
eliran zach <thehobbit85@gmail.com>
2015-06-11 03:08:13 +0300

    Improvments to tests

--- test/test.js
+++ test/test.js
@@ -21,6 +21,7 @@ var deleteFolderRecursive = function (path) {
     fs.rmdirSync(path)
   }
 }
+var fileNames = []
 
 describe('Check Transaction from raw data', function () {
   this.timeout(0)
@@ -28,22 +29,32 @@ describe('Check Transaction from raw data', function () {
     var m = Math.floor(Math.random() * folderAmountRand)
     var data = __dirname + '/data'
     var jump = false
+    var newFolder = true
     fs.mkdirSync(data)
     for (var i = 0; i < m; i++) {
       var n = Math.floor(Math.random() * fileAmountRand)
       if (Math.random() > 0.5) {
         data = data + '/data' + i
         jump = true
+        newFolder = true
         fs.mkdirSync(data)
       }
-      for (var j = 0; j < n; j++) {
+      var fileNamesTemp = []
+      for (var j = 0; j < n && newFolder; j++) {
         var size = Math.floor(Math.random() * (fileSizeRand - 1) + 1)
-        fs.writeFileSync(data + '/' + j + '.tmp', new Buffer(size), 0, size - 1)
+        var fileName = (Math.random() * 10).toFixed(0)
+        if (fileNamesTemp.indexOf(fileName) === -1) {
+          fileNamesTemp.push(fileName)
+          fileNames.push(fileName)
+          // console.log(fileName, data)
+          fs.writeFileSync(data + '/' + fileName + '.tmp', new Buffer(size), 0, size - 1)
+        }
       }
       if (jump && Math.random() > 0.5) {
         data = data.slice(0, data.lastIndexOf('/data'))
         jump = false
       }
+      newFolder = false
     }
     done()
   })
@@ -66,7 +77,39 @@ describe('Check Transaction from raw data', function () {
             console.log('Room Left: ' + (result.amountToClear * -1) + ' MB')
           } else {
             console.log('Space that needs to be clear: ' + result.amountToClear + ' MB')
-          }
+          }before(function (done) {
+    var m = Math.floor(Math.random() * folderAmountRand)
+    var data = __dirname + '/data'
+    var jump = false
+    var newFolder = true
+    fs.mkdirSync(data)
+    for (var i = 0; i < m; i++) {
+      var n = Math.floor(Math.random() * fileAmountRand)
+      if (Math.random() > 0.5) {
+        data = data + '/data' + i
+        jump = true
+        newFolder = true
+        fs.mkdirSync(data)
+      }
+      var fileNamesTemp = []
+      for (var j = 0; j < n && newFolder; j++) {
+        var size = Math.floor(Math.random() * (fileSizeRand - 1) + 1)
+        var fileName = (Math.random() * 10).toFixed(0)
+        if (fileNamesTemp.indexOf(fileName) === -1) {
+          fileNamesTemp.push(fileName)
+          fileNames.push(fileName)
+          // console.log(fileName, data)
+          fs.writeFileSync(data + '/' + fileName + '.tmp', new Buffer(size), 0, size - 1)
+        }
+      }
+      if (jump && Math.random() > 0.5) {
+        data = data.slice(0, data.lastIndexOf('/data'))
+        jump = false
+      }
+      newFolder = false
+    }
+    done()
+  })
           count++
           callback(null)
         })
